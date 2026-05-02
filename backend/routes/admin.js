0452911ef8831aee1b1a95a0b88676b5787f2efa
HellzAngel <itzmeanuhere01@gmail.com>
2026-05-02 09:54:32 +0530

    Fix: Corrected auth middleware path in admin routes

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 // Middleware to check if user is admin
 const admin = (req, res, next) => {
